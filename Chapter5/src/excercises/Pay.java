86fda7e3bb79e3546269f3a9d01e2a34bae9432a
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2018-09-26 09:49:22 -0500

    worked on Pay

--- Chapter5/src/excercises/Pay.java
+++ Chapter5/src/excercises/Pay.java
@@ -1,10 +1,33 @@
 package excercises;
 
+import java.util.Scanner;
+
 public class Pay {
 
 	public static void main(String[] args) {
-		 System.out.print("You have hours, let's see what you're worth.");
-
+		double payRate;
+		double hoursWorked;
+		int insuranceOption;
+		int skillLevel;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your skill level? >>");
+		skillLevel = input.nextInt();
+		
+		System.out.println("How many hours did you work? >>");
+		hoursWorked = input.nextInt();
+		
+		if(skillLevel == 1) 
+		{
+			insuranceOption = 0;
+			payRate = 17;
+			displayTotalPay(hoursWorked, insuranceOption, payRate);
+			
+		}
 	}
-
+	public static void displayTotalPay(double hoursWorked, int insuranceOption, double payRate) {
+		
+	}
+	
 }
+		
